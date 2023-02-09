832ee58cac07e50e6e800ce56eac6d0eabed54e0
debamoyb <debamoyb@gmail.com>
2023-02-10 04:07:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,26 +47,41 @@ technology.addEventListener("click", () => {
     let name = countryCode.value;
     getNews("technology", name)
 })
-
-getNews("", "in")
-
-function getNews(category, country) {
-    fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=cc73112990714b88bd5e117f15741e16`)
-        .then((res) => res.json())
-        .then((data) => {
-
-            let array = data.articles
-            let articles = array
+async function getNews(category, country) {
+    let res = await fetch(`https://saurav.tech/NewsAPI/top-headlines/category/${category}/${country}.json`)
+    let jsonData = await res.json()
+    console.log(jsonData)
+    let data = await jsonData.articles;
             let html = ""
-            articles.forEach(element => {
-                html += ` <div class="news_card"> <img src="${element.urlToImage}"> <div class="news_text"> <span class="title"> ${element.title} </span> 
-            <span class="author"> <b>source:</b> ${element.source.name} / ${element.publishedAt}</span> 
-            <div class="lower_text"><span class="description"> ${element.description} </span><span class="read_more"> Read more at <a href="${element.url}" target="_blank"> ${element.source.name}</a> </span></div></div>
-            </div>`
-            });
+            for(let i = 0; i< 20;i++){
+                html += ` <div class="news_card"> <img src="${data[i].urlToImage}"> <div class="news_text"> <span class="title"> ${data[i].title} </span> 
+                <span class="author"> <b>source:</b> ${data[i].source.name} / ${data[i].publishedAt}</span> 
+                <div class="lower_text"><span class="description"> ${data[i].description} </span><span class="read_more"> Read more at <a href="${data[i].url}" target="_blank"> ${data[i].source.name}</a> </span></div></div>
+                </div>`
+            }
             content.innerHTML = html
-        })
 }
+getNews("general", "in")
+
+// getNews("", "in")
+
+// function getNews(category, country) {
+//     fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=cc73112990714b88bd5e117f15741e16`)
+//         .then((res) => res.json())
+//         .then((data) => {
+
+//             let array = data.articles
+//             let articles = array
+//             let html = ""
+//             articles.forEach(element => {
+//                 html += ` <div class="news_card"> <img src="${element.urlToImage}"> <div class="news_text"> <span class="title"> ${element.title} </span> 
+//             <span class="author"> <b>source:</b> ${element.source.name} / ${element.publishedAt}</span> 
+//             <div class="lower_text"><span class="description"> ${element.description} </span><span class="read_more"> Read more at <a href="${element.url}" target="_blank"> ${element.source.name}</a> </span></div></div>
+//             </div>`
+//             });
+//             content.innerHTML = html
+//         })
+// }
 let deleteMenu = document.getElementById("header_menu")
 
 button.addEventListener("click", () => {
